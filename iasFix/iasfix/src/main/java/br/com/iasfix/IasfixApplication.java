9614f31e985b7b858489e46e3f1406ac2192d6f3
verniniGui <verninigui91@gmail.com>
2023-04-05 13:50:59 -0300

    First commit - Adicionado o esquele do projeto e suas dependencias

---
+++ iasFix/iasfix/src/main/java/br/com/iasfix/IasfixApplication.java
@@ -0,0 +1,13 @@
+package br.com.iasfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IasfixApplication.class, args);
+	}
+
+}
